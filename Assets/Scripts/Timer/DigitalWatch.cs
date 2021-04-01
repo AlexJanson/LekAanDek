b45a972f7701bf7e2e9a2b494db365ce59db2e4b
SjorsMaster <sjorspnk@gmail.com>
2021-04-01 13:51:24 +0200

    Updated script to fit vertical text, added new font with centered dots, reapplied materials with new font, generated new prefabs

--- Assets/Scripts/Timer/DigitalWatch.cs
+++ Assets/Scripts/Timer/DigitalWatch.cs
@@ -29,9 +29,13 @@ namespace LekAanDek.Timer
         [Tooltip("Show 'TI:ME' instead of '00:00'.")]
         [SerializeField]
         private bool _enableTimeText;
-        [Tooltip("The text that shows up.")]
+        [Tooltip("The text that shows up. Use <br> for a new line.")]
+        //This is done because \n doesn't work in a string as it doesn't process the newline
         [SerializeField]
-        private string _timeText = "TI:JD";
+        private string _timeText = "TI<br>¨<br>JD";
+        [Tooltip("How the dots are displayed. Use <br> for a new line.")]
+        [SerializeField]
+        private string[] _dotStyle = { "<br> <br>", "<br>¨<br>" };
 
         protected override void Update()
         {
@@ -67,7 +71,7 @@ namespace LekAanDek.Timer
         {
             _watchMaterial.color = _timeOverColor;
             if (_enableTimeText)
-                _text.text = _timeText;
+                _text.text = _timeText.Replace("<br>", "\n");
         }
 
         //Blinking
@@ -77,9 +81,9 @@ namespace LekAanDek.Timer
         }
         IEnumerator Blink()
         {
-            dots = " ";
+            dots = _dotStyle[0].Replace("<br>", "\n");
             yield return new WaitForSeconds(1f);
-            dots = ":";
+            dots = _dotStyle[1].Replace("<br>", "\n");
         }
         private void BlinkHandler()
         {
