bdd7af4c3fbcddc9e61a72023078b960645fa9ba
SjorsMaster <sjorspnk@gmail.com>
2021-04-12 09:23:16 +0200

    ﻿This push is so I can test on my VR capable hardware: A lot has happened, added small animations, structual code (No comments yet), setup of the scene, the keypad display also showing colours, and a lot more.

---
+++ Assets/Scripts/KeyCode/KeyPad.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LekAanDek.Variables;
+using LekAanDek.Events;
+
+namespace LekAanDek.KeyCode
+{
+    public class KeyPad : MonoBehaviour
+    {
+        [SerializeField]
+        private StringVariable _currentInput;
+        [SerializeField]
+        private StringVariable _correctCode;
+        [SerializeField]
+        private VoidEvent _unlockDoor, _update;
+        [SerializeField]
+        private BoolEvent _attemptSuccesful;
+        private bool _unlocked;
+
+        void Start() => Clear();
+
+        public void RemoveKey()
+        {
+            if (_currentInput.Value.Length > 0)
+                _currentInput.Value = _currentInput.Value.Remove(_currentInput.Value.Length - 1);
+            _update.Raise();
+        }
+
+        public void CheckCode()
+        {
+            if (_currentInput == _correctCode)
+            {
+                _attemptSuccesful.Raise(true);
+                _unlocked = true;
+            }
+            else
+            {
+                _attemptSuccesful.Raise(false);
+                Clear();
+            }
+            _update.Raise();
+        }
+
+        public void InputKey(string _input)
+        {
+            if (_currentInput.Value.Length < _correctCode.Value.Length)
+                _currentInput.Value = _currentInput.Value + _input;
+            else
+                _attemptSuccesful.Raise(false);
+            _update.Raise();
+        }
+
+        public void Clear()
+        {
+            _currentInput.Value = "";
+            _update.Raise();
+        }
+    }
+}
---
+++ Assets/Scripts/KeyCode/PadDisplay.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using LekAanDek.Variables;
+using LekAanDek.Collections;
+
+namespace LekAanDek.KeyCode
+{
+    public class PadDisplay : MonoBehaviour
+    {
+        //Used to fetch the length
+        [SerializeField]
+        private StringVariable _correctCode, _currentCode;
+        [SerializeField]
+        private bool _completed;
+        [SerializeField]
+        private ColorCollection _colors;
+        [SerializeField]
+        private Text _display;
+        [SerializeField]
+        private string EmptyPoints = "-  ";
+        [SerializeField]
+        private Animator _feedback;
+
+        // Start is called before the first frame update
+        public void Attempt(bool input)
+        {
+            if (input)
+                CorrectPin();
+            else
+                WrongPin();
+            UpdatePin();
+        }
+
+        string AddDashes()
+        {
+            int tmp1 = Mathf.Abs(_correctCode.Value.Length - _currentCode.Value.Length);
+            string tmp2 = "";
+            for (int i = 0; i < Mathf.Abs(tmp1 - _correctCode.Value.Length); i++)
+            {
+                tmp2 = tmp2 + $"{_currentCode.Value[i]} ";
+            }
+            for (int i = 0; i < tmp1; i++)
+            {
+                tmp2 = tmp2 + $"<color=#{ColorUtility.ToHtmlStringRGB(_colors[i + _currentCode.Value.Length])}>{EmptyPoints}</color>";
+            }
+            return tmp2;
+        }
+
+        // Update is called once per frame
+        public void UpdatePin()
+        {
+            _display.text = $" {AddDashes()}";
+        }
+
+        void WrongPin()
+        {
+            _feedback.SetTrigger("Wiggle");
+            //Flicker red and wiggle
+        }
+
+        void CorrectPin()
+        {
+            _feedback.SetTrigger("Blink");
+            //Make display green
+        }
+    }
+}
