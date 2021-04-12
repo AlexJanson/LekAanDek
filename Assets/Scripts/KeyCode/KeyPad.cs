375980ad2c0c9a48e189a80167ea67267957f3b0
DESKTOP-SJORS\sjors <sjorspnk@gmail.com>
2021-04-12 16:05:28 +0200

    Finished the keypad prototype, this keypad works with all kinds of sizes, just make sure it matches the length of the colours. A prefab has been made, all required is a code generator, but that's not for this branch ;)

--- Assets/Scripts/KeyCode/KeyPad.cs
+++ Assets/Scripts/KeyCode/KeyPad.cs
@@ -1,55 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using LekAanDek.Variables;
 using LekAanDek.Events;
 
 namespace LekAanDek.KeyCode
 {
+    /// <summary>
+    /// The behind the scenes of the keypad, it compares values, receives input and invokes conditions.
+    /// </summary>
     public class KeyPad : MonoBehaviour
     {
-        [SerializeField]
+        [SerializeField] [Tooltip("The current playerinput")]
         private StringVariable _currentInput;
-        [SerializeField]
+        [SerializeField] [Tooltip("The correct code to unlock")]
         private StringVariable _correctCode;
-        [SerializeField]
+        [SerializeField] [Tooltip("An event to unlock the door and update the display")]
         private VoidEvent _unlockDoor, _update;
-        [SerializeField]
+        [SerializeField] [Tooltip("A bool to tell the display wether the attempt was succesful or not")]
         private BoolEvent _attemptSuccesful;
-        private bool _unlocked;
+        //Local bool to stop it from tracking once it's over.
+        bool _unlocked;
 
+        //Make sure the playerinput is empty before beginning
         void Start() => Clear();
 
         public void RemoveKey()
         {
-            if (_currentInput.Value.Length > 0)
-                _currentInput.Value = _currentInput.Value.Remove(_currentInput.Value.Length - 1);
-            _update.Raise();
+            if (!_unlocked)
+            {
+                if (_currentInput.Value.Length > 0)
+                    _currentInput.Value = _currentInput.Value.Remove(_currentInput.Value.Length - 1);
+                else
+                    _attemptSuccesful.Raise(false);
+                _update.Raise();
+            }
         }
 
         public void CheckCode()
         {
-            if (_currentInput == _correctCode)
+            if (!_unlocked)
             {
-                _attemptSuccesful.Raise(true);
-                _unlocked = true;
+                if (_currentInput.Value == _correctCode.Value)
+                {
+                    _attemptSuccesful.Raise(true);
+                    _unlockDoor.Raise();
+                    _unlocked = true;
+                }
+                else
+                {
+                    _attemptSuccesful.Raise(false);
+                    Clear();
+                }
+                _update.Raise();
             }
-            else
-            {
-                _attemptSuccesful.Raise(false);
-                Clear();
-            }
-            _update.Raise();
         }
 
         public void InputKey(string _input)
         {
-            print(_input);
-            if (_currentInput.Value.Length < _correctCode.Value.Length)
-                _currentInput.Value = _currentInput.Value + _input;
-            else
-                _attemptSuccesful.Raise(false);
-            _update.Raise();
+            if (!_unlocked)
+            {
+                if (_currentInput.Value.Length < _correctCode.Value.Length)
+                    _currentInput.Value = _currentInput.Value + _input;
+                else
+                    _attemptSuccesful.Raise(false);
+                _update.Raise();
+            }
         }
 
         public void Clear()
--- Assets/Scripts/KeyCode/PadDisplay.cs
+++ Assets/Scripts/KeyCode/PadDisplay.cs
@@ -1,38 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using LekAanDek.Variables;
 using LekAanDek.Collections;
 
 namespace LekAanDek.KeyCode
 {
+    /// <summary>
+    /// This is a script that show the current display input as well as managing colors for the unfilled fields.
+    /// </summary>
     public class PadDisplay : MonoBehaviour
     {
-        //Used to fetch the length
-        [SerializeField]
+        [SerializeField] [Tooltip("The correct code to unlock and the current playerinput.")]
         private StringVariable _correctCode, _currentCode;
-        [SerializeField]
-        private bool _completed;
-        [SerializeField]
+        [SerializeField] [Tooltip("A collection of colors which will appear in order at the display. [!]Note that if there are more characters than colors that the text will turn black.")]
         private ColorCollection _colors;
-        [SerializeField]
+        [SerializeField] [Tooltip("The textobject of the screen.")]
         private Text _display;
-        [SerializeField]
-        private string EmptyPoints = "-  ";
-        [SerializeField]
+        [SerializeField] [Tooltip("The value that fills the empty spots (Spaces are reccomended for matching seperation.)")]
+        private string emptyPoints = "-  ";
+        [SerializeField] [Tooltip("The animator to give the player a visual feedback.")]
         private Animator _feedback;
 
-        // Start is called before the first frame update
         public void Attempt(bool input)
         {
             if (input)
                 CorrectPin();
             else
                 WrongPin();
-            UpdatePin();
+            UpdateDisplay();
         }
 
+        //A small function that adds the numbers and colors to the inputfield,
+        //[!] This also reverts to black if there aren't enough colours to fill the lenght of the generated code.
         string AddDashes()
         {
             int tmp1 = Mathf.Abs(_correctCode.Value.Length - _currentCode.Value.Length);
@@ -43,27 +42,17 @@ namespace LekAanDek.KeyCode
             }
             for (int i = 0; i < tmp1; i++)
             {
-                tmp2 = tmp2 + $"<color=#{ColorUtility.ToHtmlStringRGB(_colors[i + _currentCode.Value.Length])}>{EmptyPoints}</color>";
+                tmp2 = tmp2 + $"<color=#{(_colors.List.Count >= _correctCode.Value.Length ? ColorUtility.ToHtmlStringRGB(_colors[i + _currentCode.Value.Length]) : "000000")}>{emptyPoints}</color>";
             }
             return tmp2;
         }
 
-        // Update is called once per frame
-        public void UpdatePin()
-        {
-            _display.text = $" {AddDashes()}";
-        }
+        public void UpdateDisplay() => _display.text = $" {AddDashes()}";
 
-        void WrongPin()
-        {
-            _feedback.SetTrigger("Wiggle");
-            //Flicker red and wiggle
-        }
+        void WrongPin() => _feedback.SetTrigger("Wiggle");
+        //You might want to add a sound here once we start working on that
 
-        void CorrectPin()
-        {
-            _feedback.SetTrigger("Blink");
-            //Make display green
-        }
+        void CorrectPin() => _feedback.SetTrigger("Blink");
+        //You might want to add a sound here once we start working on that
     }
 }
