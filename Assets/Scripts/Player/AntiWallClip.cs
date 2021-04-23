2ee45cb298c2a7e7efe682950a324f2da2f6f7cd
Mitchell-T <mitchelltuinstra2000@gmail.com>
2021-04-23 12:46:19 +0200

    Added player pusback when trying to look through doors and walls

--- Assets/Scripts/Player/AntiWallClip.cs
+++ Assets/Scripts/Player/AntiWallClip.cs
@@ -13,6 +13,13 @@ namespace LekAanDek.Player
         [SerializeField]
         private Transform _VRHead;
         [SerializeField]
+        [Tooltip("Distance from the wall at which the position is stored")]
+        private float _saveDistance = 0.18f;
+        [SerializeField]
+        [Tooltip("Distance from the wall at which the player is pushed back toward the previous position")]
+        private float _teleportDistance = 0.12f;
+        [SerializeField]
+        [Tooltip("Distance the player is pushed back in the direction of the previously stored position")]
         private float _pushBackDistance = 0.06f;
         [SerializeField]
         private LayerMask _wallLayer;
@@ -22,15 +29,21 @@ namespace LekAanDek.Player
 
         private void Update()
         {
-            // Check if player is close to wall and store position if they are
-            if (Physics.CheckSphere(_VRHead.position, 0.18f, _wallLayer) && !_isInCollider)
+            // Check if player is close to the wall and store position if they are
+            if (Physics.CheckSphere(_VRHead.position, _saveDistance, _wallLayer) && !_isInCollider)
             {
                 Debug.Log("first sphere entered");
                 _lastPos = _VRHead.position;
                 _isInCollider = true;
-            } 
+            }
+            // Reset check if player moves away from the wall
+            else if (!Physics.CheckSphere(_VRHead.position, _saveDistance, _wallLayer) && _isInCollider)
+            {
+                Debug.Log("RESET");
+                _isInCollider = false;
+            }
             // Move player back to previously stored position if they get even closer to the wall
-            else if (Physics.CheckSphere(_VRHead.position, 0.12f, _wallLayer) && _isInCollider)
+            else if (Physics.CheckSphere(_VRHead.position, _teleportDistance, _wallLayer) && _isInCollider)
             {
                 Debug.Log("PUSHBACK");
                 Vector3 dir = (_VRHead.position - _lastPos).normalized;
