c6965dbeb0acb8b0a6ef40087ae413000a7521a0
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2021-04-26 11:34:05 +0200

    uncommented some code

--- Assets/Scripts/Watercannon/ShootWater.cs
+++ Assets/Scripts/Watercannon/ShootWater.cs
@@ -37,10 +37,10 @@ namespace LekAanDek.Puzzles.WaterCannon
 
         private void Update()
         {
-            //if (_trigger.GetState(_leftHand) || _trigger.GetState(_rightHand))
+            if (_trigger.GetState(_leftHand) || _trigger.GetState(_rightHand))
                 FiringWater();
-            //else
-                //_system.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+            else
+                _system.Stop(false, ParticleSystemStopBehavior.StopEmitting);
         }
         //In this function the particle system wil be called to play and fire particles that with go down at a given speed
         private void FiringWater()
