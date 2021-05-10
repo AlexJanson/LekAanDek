e7a7c8dd25eb9cbc2146c5a1f3455deedcef59ea
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2021-05-10 13:46:44 +0200

    Small changes

--- Assets/Scripts/Puzzles/Watercannon/AllyShip.cs
+++ Assets/Scripts/Puzzles/Watercannon/AllyShip.cs
@@ -15,7 +15,7 @@ namespace LekAanDek.Puzzles.WaterCannon
         {
             _targets[0].transform.position = new Vector3(Random.Range(-7.5f, -9.5f), Random.Range(-2.25f, 1.25f), 0);
             _targets[1].transform.position = new Vector3(Random.Range(-2f, -4f), Random.Range(-2.25f, 1.25f), 0);
-            _targets[2].transform.position = new Vector3(Random.Range(-7.5f, -9.5f), Random.Range(-2.25f, 1.25f), 0);
+            _targets[2].transform.position = new Vector3(Random.Range(2.5f, 5.3f), Random.Range(-2.25f, 1.25f), 0);
             _targets[3].transform.position = new Vector3(Random.Range(-7.5f, -9.5f), Random.Range(-2.25f, 1.25f), 0);
         }
 
