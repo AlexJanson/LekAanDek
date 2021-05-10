fa648568a672047594a48a03948fcb4fc08a93ed
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2021-05-10 16:48:39 +0200

    Added behavior pose to laser

--- Assets/Scripts/UI/PauseMenu/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -82,7 +82,7 @@ namespace LekAanDek.UI
         {
 
             _canvasTransform.position = new Vector3(_canvasTransform.position.x, _camTransform.position.y, _camTransform.position.z);
-            _canvasTransform.rotation = Quaternion.Euler(_canvasTransform.rotation.x, _camTransform.eulerAngles.y + 90, _canvasTransform.rotation.z);
+            _canvasTransform.rotation = Quaternion.Euler(_canvasTransform.rotation.x, _camTransform.eulerAngles.y, _canvasTransform.rotation.z);
 
 
             if (_clickAction.GetStateDown(_rightHand) == true || Input.GetKeyDown("p"))
