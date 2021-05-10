41227507cd4994cdaed3edccdbb17a5778be4fdf
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2021-05-10 14:25:48 +0200

    Changed Fade start to Fade view

--- Assets/Scripts/UI/Fade.cs
+++ Assets/Scripts/UI/Fade.cs
@@ -16,23 +16,36 @@ namespace LekAanDek.UI
         [SerializeField]
         private float _buffer = 1f;
 
+        private bool _startFading = false;
+
+        [SerializeField]
+        private Camera _camera;
+
         private void Start()
         {
             StartCoroutine(WaitCoroutine());
         }
+        private void Update()
+        {
+            if(_startFading == true)
+            {
+
+            }
+        }
+
         private void FadeTo()
         {
             //set start color
-            SteamVR_Fade.Start(Color.clear, 0f);
+            SteamVR_Fade.View(Color.clear, 0f);
             //set and start fade to
-            SteamVR_Fade.Start(Color.black, _fadeDuration);
+            SteamVR_Fade.View(Color.black, _fadeDuration);
         }
         private void FadeFrom()
         {
             //set start color
-            SteamVR_Fade.Start(Color.black, 1f);
+            SteamVR_Fade.View(Color.black, 1f);
             //set and start fade to
-            SteamVR_Fade.Start(Color.clear, _fadeDuration);
+            SteamVR_Fade.View(Color.clear, _fadeDuration);
         }
 
         IEnumerator WaitCoroutine()
