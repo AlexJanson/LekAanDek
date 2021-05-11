e05322122b17072942517aa666e9ef26f73f9f55
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2021-05-11 11:01:37 +0200

    Changed target color

--- Assets/Scripts/Puzzles/Watercannon/AllyShip.cs
+++ Assets/Scripts/Puzzles/Watercannon/AllyShip.cs
@@ -13,10 +13,7 @@ namespace LekAanDek.Puzzles.WaterCannon
         // Start is called before the first frame update
         void Start()
         {
-            _targets[0].transform.position = new Vector3(Random.Range(-7.5f, -9.5f), Random.Range(-3f, 10f), _targets[0].transform.position.z);
-            _targets[1].transform.position = new Vector3(Random.Range(-2f, -4f), Random.Range(-3f, 10f), _targets[1].transform.position.z);
-            _targets[2].transform.position = new Vector3(Random.Range(2.5f, 5.3f), Random.Range(-3f, 10f), _targets[2].transform.position.z);
-            _targets[3].transform.position = new Vector3(Random.Range(8.5f, 10.5f), Random.Range(-3f, 10f), _targets[3].transform.position.z);
+            
         }
 
         // Update is called once per frame
